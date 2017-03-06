bf24359a67d45ff05691bfcc1c5b69490c2bcbae
Toubic <tj22fe@student.lnu.se>
2017-03-07 00:51:30 +0100

    config + routes

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ var mongoose = require("mongoose");
 try {
 
     var app = express();
+    var config = require("./config/config");
+    var dashboard = require("./routes/dashboard");
+    var options = require("./routes/options");
 
     if (config.database.credentials === undefined)
         throw new Error("No database credentials given");
 
     app.use(bodyParser.json());
+
+    app.use('/dashboard', dashboard);
+    app.use('/options', options);
 
     app.listen(process.env.PORT || 5000);
 
@@ -19,7 +25,7 @@ try {
     var db = mongoose.connection;
 
     app.get("/", function (req, res) {
-
+        res.send("test");
     });
 
 }
