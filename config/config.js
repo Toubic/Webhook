05626de0302ae5b15177d46dda592a89c353a971
Toubic <tj22fe@student.lnu.se>
2017-03-14 07:23:17 +0100

    + config for heroku

---
+++ config/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+
+    database: {
+        credentials: 'mongodb://admin:_1dv612_@ds015636.mlab.com:15636/1dv612'
+    },
+    passport: {
+        GITHUB_CLIENT_ID: '6178b9f3b53db0ab8483',
+        GITHUB_CLIENT_SECRET: '56276fb7623911835ca83e58b318877b2a4e7e1d'
+    }
+
+};
