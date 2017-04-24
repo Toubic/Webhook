08566c805345f9c95050d43fff702923be9353bb
Toubic <tj22fe@student.lnu.se>
2017-04-24 05:28:11 +0200

    + octocat

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,26 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var orgs = require("github-user-orgs");
+var GitHub = require("octocat");
 
 router.get("/",
     passport.authenticate('github', { failureRedirect: 'https://github.com/' }),
     function(req, res) {
-
+        var client = new GitHub({
+            token: req.user.accessToken
+        });
+        client.post('/orgs/exam2/hooks', {
+            "name": "web",
+            "active": true,
+            "events": [
+                "push",
+                "pull_request"
+            ],
+            "config": {
+                "url": "http://example.com/webhook",
+                "content_type": "json"
+            }
+        });
         var opts = {
             "username": req.user.profile.username
         };
